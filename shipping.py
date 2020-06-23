6d0d5ceadc35e8e850991df4d28b69e8c632e63b
Akshay Mishra <42304248+Jontyabhay@users.noreply.github.com>
2020-06-24 02:30:10 +0530

    Create shipping.py

---
+++ shipping.py
@@ -0,0 +1,34 @@
+def ground_cost(weight):
+  if (weight <= 2):
+    return 1.5*weight+20
+  elif (weight>2) and (weight<=6):
+    return 3.00*weight+20
+  elif (weight>6) and (weight<=10):
+    return 4.00*weight+20
+  elif (weight>10):
+    return 4.75*weight+20
+
+print(ground_cost(8.4))
+premium_ground = 125
+
+def drone_cost(weight):
+  if (weight <= 2):
+    return weight*4.5
+  elif (weight>2) and (weight<=6):
+    return weight*9.00
+  elif (weight>6) and (weight<=10):
+    return weight*12.00
+  elif (weight>10):
+    return weight*14.25
+
+print(drone_cost(1.5))
+
+def cheap_shipping(weight):
+  if(ground_cost(weight) > drone_cost(weight)) and (ground_cost(weight)>125):
+    return 'Drone shipping is cheapest with ' + str(drone_count(weight))
+  elif (ground_cost(weight) < drone_cost(weight)):
+    return "Ground shipping is cheapest with " + str(ground_cost(weight))
+
+print(cheap_shipping(4.8))  
+
+
